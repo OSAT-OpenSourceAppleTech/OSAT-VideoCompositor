d72fa329654fb6fb2b91327678f6066a3e3a47a2
hdutt <hdutt@cisco.com>
2022-12-17 23:31:54 +0530

    Added structures for Annotation objects Added Error enums Added a Composition skeleton class

---
+++ OSATAnnotation.swift
@@ -0,0 +1,104 @@
+//
+//  OSATAnnotation.swift
+//  OSAT-VideoCompositor
+//
+//  Created by hdutt on 17/12/22.
+//
+
+import AVFoundation
+
+/// OSATAnnotationProtocol  is a base protocol for all Annotation object layers supported by OSAT-VideoCompositor
+protocol OSATAnnotationProtocol {
+    func getAnnotationLayer() -> CALayer
+}
+
+/// OSATImageAnnotation holds data for  the image annotation to be rendered on Video
+public struct OSATImageAnnotation: OSATAnnotationProtocol {
+    let image: UIImage
+    let frame: CGRect
+    let timeRange: CMTimeRange
+    let caption: NSString?
+    let attributedCaption: NSAttributedString?
+    
+    public init(image: UIImage, frame: CGRect, timeRange: CMTimeRange, caption: NSString?, attributedCaption: NSAttributedString?) {
+        self.image = image
+        self.frame = frame
+        self.timeRange = timeRange
+        self.caption = caption
+        self.attributedCaption = attributedCaption
+    }
+    
+    public func getAnnotationLayer() -> CALayer {
+        let imageLayer = CALayer()
+        imageLayer.frame = frame
+        imageLayer.contents = image.cgImage
+        return imageLayer
+    }
+}
+
+
+/// OSATTextAnnotation holds data for the text annotation to be rendered on Video
+public struct OSATTextAnnotation: OSATAnnotationProtocol {
+    let text: NSString
+    let frame: CGRect
+    let timeRange: CMTimeRange
+    let attributedText: NSAttributedString?
+    let textColor: UIColor?
+    let backgroundColor: UIColor?
+    let font: UIFont?
+    
+    public init(text: NSString, frame: CGRect, timeRange: CMTimeRange, attributedText: NSAttributedString?, textColor: UIColor?, backgroundColor: UIColor?, font: UIFont?) {
+        self.text = text
+        self.frame = frame
+        self.timeRange = timeRange
+        self.attributedText = attributedText
+        self.textColor = textColor
+        self.backgroundColor = backgroundColor
+        self.font = font
+    }
+
+    public func getAnnotationLayer() -> CALayer {
+        let textLayer = CATextLayer()
+        textLayer.string = attributedText ?? text
+        textLayer.font = font ?? UIFont(name: "Helvetica", size: CGFloat(20))
+        textLayer.backgroundColor = backgroundColor?.cgColor ?? UIColor.clear.cgColor
+        textLayer.frame = frame
+
+        textLayer.shouldRasterize = true
+        textLayer.rasterizationScale = UIScreen.main.scale
+        textLayer.backgroundColor = UIColor.clear.cgColor
+        textLayer.alignmentMode = .center
+        textLayer.displayIfNeeded()
+        return textLayer
+    }
+}
+
+
+/// OSATBezierAnnotation holds data for the Bezier sPath annotation to be rendered on Video
+public struct OSATBezierAnnotation: OSATAnnotationProtocol {
+    let bezierPath: UIBezierPath
+    let position: CGPoint
+    let lineWidth: CGFloat
+    let timeRange: CMTimeRange
+    let strokeColor: UIColor?
+    let fillColor: UIColor?
+    
+    public init(bezierPath: UIBezierPath, position: CGPoint, lineWidth: CGFloat, timeRange: CMTimeRange, strokeColor: UIColor?, fillColor: UIColor?) {
+        self.bezierPath = bezierPath
+        self.position = position
+        self.lineWidth = lineWidth
+        self.timeRange = timeRange
+        self.strokeColor = strokeColor
+        self.fillColor = fillColor
+    }
+    
+    public func getAnnotationLayer() -> CALayer {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = bezierPath.cgPath
+        shapeLayer.position = position
+        shapeLayer.lineWidth = lineWidth
+        shapeLayer.strokeColor = strokeColor?.cgColor ?? UIColor.black.cgColor
+        shapeLayer.fillColor = strokeColor?.cgColor ?? UIColor.clear.cgColor
+        return shapeLayer
+    }
+}
---
+++ OSATVideoCompositionErrors.swift
@@ -0,0 +1,13 @@
+//
+//  OSATVideoCompositionErrors.swift
+//  OSAT-VideoCompositor
+//
+//  Created by hdutt on 17/12/22.
+//
+
+import Foundation
+
+public enum OSATVideoCompositionError {
+    case videoAssetCorrupt
+    case assetExportSessionFailed
+}
