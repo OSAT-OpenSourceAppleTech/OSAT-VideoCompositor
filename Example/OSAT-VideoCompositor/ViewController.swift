9a1962de31c4a75c9f3835ee70ddb0e49e218fcb
Urmit Chauhan <urmitc96@gmail.com>
2023-05-03 23:34:25 +0530

    update viewcontroller

--- Example/OSAT-VideoCompositor/ViewController.swift
+++ Example/OSAT-VideoCompositor/ViewController.swift
@@ -12,8 +12,8 @@ import UIKit
 
 class ViewController: UIViewController {
     private struct Constants {
-        static let playButton = "play"
-        static let pauseButton = "pause"
+        static let playButton = "play.circle.fill"
+        static let pauseButton = "pause.circle.fill"
         static let iconSize: CGFloat = 40
     }
     
@@ -84,7 +84,7 @@ class ViewController: UIViewController {
         let url = Bundle.main.url(forResource: "videoplayback", withExtension: "mp4")!
         videoPlayerLayer = AVPlayerView(frame: .zero)
         originalVideoUrl = url
-        view.backgroundColor = .black
+        view.backgroundColor = .systemBackground
         
         videoPlayerLayer?.set(url: url)
         videoPlayerLayer?.delegate = self
@@ -98,9 +98,9 @@ class ViewController: UIViewController {
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: nil)
         navigationItem.leftBarButtonItem?.menu = createWaterMarkMenu()
+        navigationController?.navigationBar.barStyle = .default
         
-        videoPlayerLayer.backgroundColor = .systemGray
-        videoPlayerLayer.play()
+        videoPlayerLayer.backgroundColor = .systemGroupedBackground
         addSubviews()
         setButtonProperties()
         setupConstraints()
@@ -126,10 +126,9 @@ class ViewController: UIViewController {
             playerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             playerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             playerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            playerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -300),
             
             videoPlayerLayer.centerXAnchor.constraint(equalTo: playerView.centerXAnchor),
-            videoPlayerLayer.centerXAnchor.constraint(equalTo: playerView.centerXAnchor),
+            videoPlayerLayer.centerYAnchor.constraint(equalTo: playerView.centerYAnchor),
             videoPlayerLayer.leadingAnchor.constraint(equalTo: playerView.leadingAnchor),
             videoPlayerLayer.trailingAnchor.constraint(equalTo: playerView.trailingAnchor),
             videoPlayerLayer.heightAnchor.constraint(equalTo: videoPlayerLayer.widthAnchor, multiplier: 1),
@@ -138,17 +137,20 @@ class ViewController: UIViewController {
             spinner.centerXAnchor.constraint(equalTo: playerView.centerXAnchor),
             
             sliderParentView.topAnchor.constraint(equalTo: playerView.bottomAnchor, constant: 10),
-            sliderParentView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -60),
-            sliderParentView.leadingAnchor.constraint(equalTo: videoPlayerLayer.leadingAnchor),
-            sliderParentView.trailingAnchor.constraint(equalTo: videoPlayerLayer.trailingAnchor),
+            sliderParentView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+            sliderParentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            sliderParentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            sliderParentView.heightAnchor.constraint(equalToConstant: 100.0),
             
-            slider.leadingAnchor.constraint(equalTo: sliderParentView.leadingAnchor, constant: 10),
-            slider.trailingAnchor.constraint(equalTo: sliderParentView.trailingAnchor, constant: -10),
-            slider.heightAnchor.constraint(equalTo: sliderParentView.heightAnchor),
-            
-            playbutton.topAnchor.constraint(equalTo: sliderParentView.topAnchor, constant: 20),
+            playbutton.centerYAnchor.constraint(equalTo: sliderParentView.centerYAnchor),
+            playbutton.leadingAnchor.constraint(equalTo: sliderParentView.safeAreaLayoutGuide.leadingAnchor),
             playbutton.heightAnchor.constraint(equalToConstant: Constants.iconSize),
             playbutton.widthAnchor.constraint(equalToConstant: Constants.iconSize),
+            
+            slider.centerYAnchor.constraint(equalTo: sliderParentView.centerYAnchor),
+            slider.leadingAnchor.constraint(equalTo: playbutton.safeAreaLayoutGuide.trailingAnchor, constant: 10),
+            slider.trailingAnchor.constraint(equalTo: sliderParentView.safeAreaLayoutGuide.trailingAnchor, constant: -10)
+            
         ])
     }
     
@@ -229,7 +231,7 @@ class ViewController: UIViewController {
         }
         
         let deferredMenu1 = UIDeferredMenuElement { (menuElements) in
-            let menu = UIMenu(title: "Editor Example", options: .displayInline,  children: [multiVideo])
+            let menu = UIMenu(title: "Feature Example", options: .displayInline,  children: [multiVideo])
             menuElements([menu])
         }
         
@@ -274,8 +276,8 @@ class ViewController: UIViewController {
     }
     
     private func setButtonProperties() {
-        playbutton.setImage(UIImage(systemName: Constants.playButton, withConfiguration: UIImage.SymbolConfiguration(pointSize: Constants.iconSize)), for: .selected)
         playbutton.setImage(UIImage(systemName: Constants.pauseButton, withConfiguration: UIImage.SymbolConfiguration(pointSize: Constants.iconSize)), for: .normal)
+        playbutton.setImage(UIImage(systemName: Constants.playButton, withConfiguration: UIImage.SymbolConfiguration(pointSize: Constants.iconSize)), for: .selected)
     }
     
     @objc private func handlePlayButtonAction(_ sender: Any) {
@@ -407,9 +409,6 @@ class ViewController: UIViewController {
                         self.videoPlayerLayer.isHidden = false
                         self.spinner.isHidden = true
                         self.spinner.stopAnimating()
-                        self.selectedImageSrc = nil
-                        self.fontSize = 20
-                        self.fontColor = .black
                         Task {
                             await self.getDuration()
                         }
